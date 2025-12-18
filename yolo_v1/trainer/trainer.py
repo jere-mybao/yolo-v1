dbbf59cf52e3ddcf9b019047d0c27c32771fc6f5
jere-mybao <jeremy.bao@princeton.edu>
2025-12-18 02:03:52 -0500

    integrate configs

--- yolo_v1/trainer/trainer.py
+++ yolo_v1/trainer/trainer.py
@@ -7,27 +7,32 @@ from yolo_v1.loss.loss import YoloV1Loss
 from yolo_v1.models.model import YoloV1_Resnet50
 from yolo_v1.utils.utils import mAP, get_bboxes
 import wandb
+import yaml
 
-wandb.init(project="yolo-v1", name="yolo-v1-pascal-voc")
+with open('yolo_v1/config/yolo_trainer.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+
+if config["wandb"]["enabled"]:
+    wandb.init(project=config["wandb"]["project"], name=config["wandb"]["name"])
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 gpu_count = torch.cuda.device_count()
 print(f"Using {gpu_count} GPUs")
 if gpu_count > 1:
-    batch_size = 4 * gpu_count
-    nworkers = 4 * gpu_count
+    batch_size = config["training"]["batch_size"] * gpu_count
+    nworkers = config["training"]["nworkers"] * gpu_count
 else:
-    batch_size = 4
-    nworkers = 2
+    batch_size = config["training"]["batch_size"]
+    nworkers = config["training"]["nworkers"]
 
-weight_decay = 0.0005
-epochs = 200
-lr_sched_original = False
-lr_sched_conservative = True
+weight_decay = config["optimizer"]["weight_decay"]
+epochs = config["training"]["epochs"]
+lr_sched_original = config["optimizer"]["lr_scheduler"]["original"]
+lr_sched_conservative = config["optimizer"]["lr_scheduler"]["conservative"]
 
 
 def train(model, train_loader, optimizer, loss_fn):
-    accumulation_steps = 16 
+    accumulation_steps = config["training"]["accumulation_steps"] 
     total_loss = 0
     model.train()
     for batch_idx, (x, y) in enumerate(train_loader):
@@ -55,18 +60,18 @@ def test(model, test_loader, loss_fn):
     return float(total_loss / len(test_loader))
 
 def main():
-    train_dataset = Dataset(csv_file="data/train.csv", img_dir="data/images", label_dir="data/labels", grid_size=7, num_bboxes=2, num_classes=20, additional_transform=True)
-    test_dataset = Dataset(csv_file="data/test.csv", img_dir="data/images", label_dir="data/labels", grid_size=7, num_bboxes=2, num_classes=20, additional_transform=False)
+    train_dataset = Dataset(csv_file=config["data"]["csv_file_train"], img_dir=config["data"]["img_dir"], label_dir=config["data"]["label_dir"], grid_size=config["model"]["grid_size"], num_bboxes=config["model"]["num_bboxes"], num_classes=config["model"]["num_classes"], additional_transform=config["data"]["additional_transform_train"])
+    test_dataset = Dataset(csv_file=config["data"]["csv_file_test"], img_dir=config["data"]["img_dir"], label_dir=config["data"]["label_dir"], grid_size=config["model"]["grid_size"], num_bboxes=config["model"]["num_bboxes"], num_classes=config["model"]["num_classes"], additional_transform=config["data"]["additional_transform_test"])
     
     train_loader = DataLoader[Any](train_dataset, batch_size=batch_size, shuffle=True, num_workers=nworkers)
     test_loader = DataLoader[Any](test_dataset, batch_size=batch_size, shuffle=False, num_workers=nworkers)
 
-    model = YoloV1_Resnet50(grid_size=7, num_bboxes=2, num_classes=20).to(device)
+    model = YoloV1_Resnet50(grid_size=config["model"]["grid_size"], num_bboxes=config["model"]["num_bboxes"], num_classes=config["model"]["num_classes"]).to(device)
     if gpu_count > 1:
         model = torch.nn.DataParallel(model)
 
-    optimizer = Adam(model.parameters(), lr=0.0001, weight_decay=weight_decay)
-    loss_fn = YoloV1Loss(grid_size=7, num_bboxes=2, num_classes=20)
+    optimizer = Adam(model.parameters(), lr=config["optimizer"]["learning_rate"], weight_decay=weight_decay)
+    loss_fn = YoloV1Loss(grid_size=config["model"]["grid_size"], num_bboxes=config["model"]["num_bboxes"], num_classes=config["model"]["num_classes"])
 
     train_loss  = []
     train_mAP = []
@@ -102,11 +107,11 @@ def main():
         test_loss_value = test(model, test_loader, loss_fn)
         test_loss.append(test_loss_value)
 
-        pred_bbox, target_bbox = get_bboxes(train_loader, model, iou_threshold = 0.5, threshold = 0.4)
-        test_pred_bbox, test_target_bbox = get_bboxes(test_loader, model, iou_threshold = 0.5, threshold = 0.4)   
+        pred_bbox, target_bbox = get_bboxes(train_loader, model, iou_threshold = config["training"]["iou_threshold"], threshold = config["training"]["threshold"])
+        test_pred_bbox, test_target_bbox = get_bboxes(test_loader, model, iou_threshold = config["training"]["iou_threshold"], threshold = config["training"]["threshold"])   
 
-        train_mAP_val = mAP(pred_bbox, target_bbox, iou_threshold = 0.5)
-        test_mAP_val = mAP(test_pred_bbox, test_target_bbox, iou_threshold = 0.5)
+        train_mAP_val = mAP(pred_bbox, target_bbox, iou_threshold = config["training"]["iou_threshold"])
+        test_mAP_val = mAP(test_pred_bbox, test_target_bbox, iou_threshold = config["training"]["iou_threshold"])
 
         train_mAP.append(train_mAP_val.item())
         test_mAP.append(test_mAP_val.item())
